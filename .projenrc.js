34adf4fa7e9eb9738a9ad75f1b3ec3b52f9758d0
Kane Zhu <me@kane.mx>
2021-12-03 05:25:44 +0000

    chore: bump to mininum node and cdk to v2.0.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { AwsCdkConstructLibrary, AwsCdkTypeScriptApp } = require('projen');
 
-const cdkVersion = '2.0.0-rc.27';
+const cdkVersion = '2.0.0';
 
 const tsCustomConfig = {
   exclude: ['example'],
@@ -80,7 +80,7 @@ const project = new AwsCdkConstructLibrary({
   license: 'Apache-2.0', /* License's SPDX identifier. */
   licensed: true, /* Indicates if a license should be added. */
   // maxNodeVersion: undefined,                                                /* Minimum node.js version to require via `engines` (inclusive). */
-  minNodeVersion: '14.15.0', /* Minimum Node.js version to require via package.json `engines` (inclusive). */
+  minNodeVersion: '14.17.0', /* Minimum Node.js version to require via package.json `engines` (inclusive). */
   // npmAccess: undefined,                                                     /* Access level of the npm package. */
   // npmDistTag: 'latest',                                                     /* Tags can be used to provide an alias instead of version numbers. */
   // npmRegistryUrl: 'https://registry.npmjs.org',                             /* The base URL of the npm package registry. */
@@ -195,6 +195,7 @@ examplePrj.package.addField('resolutions', {
   'pac-resolver': '^5.0.0',
   'set-value': '^4.0.1',
   'ansi-regex': '^5.0.1',
+  'json-schema': '^0.4.0',
 });
 
 project.synth();
