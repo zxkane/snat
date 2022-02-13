8b671048afc7cc67051f10c409ac1f0f150c91d2
Kane Zhu <me@kane.mx>
2022-02-13 16:36:26 +0000

    fix: update example to resolve test and eslint failures

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,8 +1,10 @@
-import { Construct } from 'constructs';
 import { App, Stack, StackProps, Arn, Aws } from 'aws-cdk-lib';
-import { SimpleNAT } from 'cdk-construct-simple-nat';
 import { Vpc, GatewayVpcEndpointAwsService, SubnetType, InstanceType, InstanceSize, InstanceClass, MachineImage, AmazonLinuxCpuType, AmazonLinuxGeneration } from 'aws-cdk-lib/aws-ec2';
 import { Role, ServicePrincipal, PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { SimpleNAT } from 'cdk-construct-simple-nat';
+import { Construct } from 'constructs';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint @typescript-eslint/no-require-imports: "off" */
 const fetch = require('sync-fetch');
 
 export class SimpleNATStack extends Stack {
@@ -24,55 +26,55 @@ export class SimpleNATStack extends Stack {
         },
       },
     });
-    
+
     if (vpc.publicSubnets.length < 1) {
       throw new Error('The VPC must have PUBLIC subnet.');
     }
-    
+
     const keyParaName = this.node.tryGetContext('RemoteKeyName') ?? 'remote-key';
     const hostParaName = this.node.tryGetContext('RemoteHostName') ?? 'remote-host';
     const role = new Role(this, 'NatRole', {
       assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
       inlinePolicies: {
         ssm: new PolicyDocument({
-            statements: [
-              new PolicyStatement({
-                actions: [
-                  'ssm:GetParameter',
-                ],
-                resources: [
-                  Arn.format({
-                    service: 'ssm',
-                    resource: 'parameter',
-                    resourceName: keyParaName,
-                  }, Stack.of(this)),
-                  Arn.format({
-                    service: 'ssm',
-                    resource: 'parameter',
-                    resourceName: hostParaName,
-                  }, Stack.of(this)),
-                ],
-              }),
-            ],
+          statements: [
+            new PolicyStatement({
+              actions: [
+                'ssm:GetParameter',
+              ],
+              resources: [
+                Arn.format({
+                  service: 'ssm',
+                  resource: 'parameter',
+                  resourceName: keyParaName,
+                }, Stack.of(this)),
+                Arn.format({
+                  service: 'ssm',
+                  resource: 'parameter',
+                  resourceName: hostParaName,
+                }, Stack.of(this)),
+              ],
+            }),
+          ],
         }),
         kms: new PolicyDocument({
-            statements: [
-              new PolicyStatement({
-                actions: [
-                  'kms:Decrypt',
-                ],
-                resources: ['*'],
-              }),
-            ],
+          statements: [
+            new PolicyStatement({
+              actions: [
+                'kms:Decrypt',
+              ],
+              resources: ['*'],
+            }),
+          ],
         }),
       },
     });
-    
+
     const IPs = [
       // '74.125.0.0/16',
       // '172.217.0.0/16',
     ];
-    
+
     const ipV6Regex = new RegExp(SimpleNAT.Ipv6Regex);
     const githubMeta = fetch('https://api.github.com/meta').json();
     for (const cidr of githubMeta.git) {
@@ -95,7 +97,7 @@ export class SimpleNATStack extends Stack {
       }),
       customScripts: `#!/bin/bash -x
 yum install -y python3-pip gcc python3-devel
-pip3 install -i https://opentuna.cn/pypi/web/simple sshuttle
+pip3 install -i https://opentuna.cn/pypi/web/simple sshuttle==1.0.5
 groupadd sshuttle
 useradd -d /home/sshuttle -g sshuttle sshuttle
 mkdir /home/sshuttle/.ssh
@@ -124,8 +126,8 @@ systemctl start sshuttle
       `,
       role,
     });
-    
-    IPs.forEach(ip => { nat.addV4Route(ip)});
+
+    IPs.forEach(ip => { nat.addV4Route(ip);});
   }
 }
 
--- example/test/main.test.ts
+++ example/test/main.test.ts
@@ -1,11 +1,11 @@
-import '@aws-cdk/assert/jest';
 import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import { SimpleNATStack } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
   const stack = new SimpleNATStack(app, 'test');
 
-  expect(stack).not.toHaveResource('AWS::S3::Bucket');
+  Template.fromStack(stack).resourceCountIs('AWS::S3::Bucket', 0);
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
